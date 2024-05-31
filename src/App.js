0495353309ba4db9b85b2b556e14bd49c09b386b
ilo yoon <iloyoon@MacBook-Pro-7.local>
2024-05-31 10:21:45 +0900

    fix build error

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import TopNav from "component/topNav"
-
-import Invest from "pages/Invest"
-import SuiLong from "pages/SuiLong"
-import SuiLongStaking from "pages/SuiLongStaking"
-
-import Poollist from "pages/poolsearch"
 import DetailStaking from "pages/Detail_Staking"
 
 // import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
-
 function App() {
   return (
     <>
-    
       <Router>
         <Routes>
             <Route path="/" element={<TopNav />} />
